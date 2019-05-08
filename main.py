ae15c7be89cd5d43d19344e2703c8314e2d5df89
Paul Hager <paul.hager94@gmail.com>
2019-05-08 16:08:22 +0200

    Feature calculation and feature hist done

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import argparse
-#import matplotlib.pyplot as plt # pylint: disable=import-error
+import matplotlib.pyplot as plt # pylint: disable=import-error
 import os
 import ntpath
 import re
+from Bio.SubsMat import MatrixInfo as matrices
 
 parser = argparse.ArgumentParser(description='Load and analyse protein binding site data')
 parser.add_argument('--fastaFolder', help = "Path to folder containing fasta files", type = str)
@@ -15,6 +16,18 @@ fastaFolder = args.fastaFolder
 snapFolder = args.snapFolder
 bindingResiduesFile = args.bindingResidues
 
+blosum62 = matrices.blosum62
+blosumCutoffsDict = {
+  -4 : 90, 
+  -3 : 80, 
+  -2 : 70, 
+  -1 : 60, 
+  0 : 50, 
+  1 : 40, 
+  2 : 30,
+  3 : 0
+}
+
 def loadFastaFiles(fastaFolder):
   proteinSeqDict = {}
   for filename in os.listdir(fastaFolder):
@@ -24,26 +37,37 @@ def loadFastaFiles(fastaFolder):
       proteinSeqDict[header[1:]] = sequence
   return proteinSeqDict
 
-def loadSnapFiles(snapFolder):
+def loadSnapCalcFeature(snapFolder, blosum62, blosumCutoffsDict):
   aaOrder = ['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V']
   snapScoreDict = {}
   snapConfDict = {}
+  featureDict = {}
   for filename in os.listdir(snapFolder):
     aaIndex = 0
     proteinID = os.path.basename(filename)[:-6]
     snapScoreDict[proteinID] = []
     snapConfDict[proteinID] = []
+    featureDict[proteinID] = []
     posAAscores = []
     posAAconf = []
+    features = []
     with open(os.path.join(snapFolder, filename), 'r') as snapFile:
       for line in snapFile:
-        posAAid = line[0]
-        if aaOrder.index(posAAid[0]) == aaIndex:
+        splitLine = line.split()
+        score = splitLine[len(splitLine)-1]
+        posAAinfo = splitLine[0]
+        posAAid = posAAinfo[0]
+        mutAAid = posAAinfo[-1]
+        posAAindex = aaOrder.index(posAAid)
+        mutAAindex = aaOrder.index(mutAAid)
+        if posAAindex == aaIndex:
           aaIndex += 1
           posAAscores.append(None)
           posAAconf.append(None)
-        splitLine = line.split()
-        score = splitLine[len(splitLine)-1]
+          features.append(None)
+        if score[-1] == '%':
+          score = score[:-1]
+        score = int(score)
         if len(posAAscores) > len(posAAconf):
           posAAconf.append(score)
           if aaIndex < 19:
@@ -51,23 +75,38 @@ def loadSnapFiles(snapFolder):
           else:
             snapScoreDict[proteinID].append(posAAscores)
             snapConfDict[proteinID].append(posAAconf)
+            featureDict[proteinID].append(features)
             posAAscores = []
             posAAconf = []
+            features = []
             aaIndex = 0
         else:
           posAAscores.append(score)
-  return snapScoreDict, snapConfDict
+          if posAAindex < mutAAindex:
+            firstAA = mutAAid
+            secondAA = posAAid
+          else:
+            firstAA = posAAid
+            secondAA = mutAAid
+          if score > blosumCutoffsDict[blosum62[(firstAA, secondAA)]]:
+            features.append(1)
+          else:
+            features.append(0)
+  return snapScoreDict, snapConfDict, featureDict
 
-
-def calcFeatureLists(proteinSeqDict, snapScoreDict):
-  print(len(proteinSeqDict))
-  print(len(snapScoreDict))
-  print(len(snapScoreDict['P12733']))
-
-  featureLists = []
-  return featureLists
-
+def createFeatureHist(featureDict):
+  featureSumFrequencies = []
+  for featureArrays in featureDict.values():
+    for featureArray in featureArrays:
+      featureSumFrequencies.append(featureArray.count(1))
+  plt.hist(x=featureSumFrequencies, bins = range(0,20), align = 'left')
+  plt.xticks(range(0,20))
+  plt.xticks(rotation=90)
+  plt.xlabel('Feature Sum')
+  plt.ylabel('Frequency')
+  plt.title('Histogram of feature sum frequencies')
+  plt.savefig("featureHistogram.pdf")
 
 proteinSeqDict = loadFastaFiles(fastaFolder)
-snapScoreDict, snapConfDict = loadSnapFiles(snapFolder)
-featureLists = calcFeatureLists(proteinSeqDict, snapScoreDict)
+snapScoreDict, snapConfDict, featureDict = loadSnapCalcFeature(snapFolder, blosum62, blosumCutoffsDict)
+createFeatureHist(featureDict)
